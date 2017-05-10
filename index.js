b858f869a2cb878e6c5bea39bb2a0c5f373053ce
Arthur Binello <arthur.binello@gmail.com>
2017-05-10 20:20:31 +0200

    Added connect and basic ping command

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+    if(message.content == 'ping'){
+        //message.reply('pong');
+        message.channel.sendMessage('pong');
+    }
+});
+
+bot.login('MzExMTgxNzI0MTQ0ODkzOTUy.C_TpYw.Ym-XFmtjCOhO3SmtFM7zhdyfFmk');
