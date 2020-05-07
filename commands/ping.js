dcc91be2db4e4ac83b248b2d703ec028d94673a3
Arthur Binello <arthur.binello@gmail.com>
2020-05-07 19:22:02 +0100

    fixed ping and node modules

--- commands/ping.js
+++ commands/ping.js
@@ -15,14 +15,33 @@ module.exports = class ping extends command {
             });
         }
         else{
-            let dateNow = Date.now() - message.createdTimestamp;
+            let APIdelay = Date.now() - message.createdTimestamp;
             let authorid = message.author.id;
-            let rgb = parseInt('0x' + parseInt(authorid % 256, 16) + parseInt(authorid % 255, 16) + parseInt(authorid % 254, 16)); //make color based on user ID
-            var embed = new Discord.RichEmbed()
-                .setTitle('ping : **' + dateNow + '** ms')
-                .setColor(rgb)
-                .setAuthor(message.author.username, message.author.avatarURL);
-            message.channel.send({embed}).then(msg => {
+
+            let colors = [0, 255, authorid % 256];
+            if(authorid % 2){
+                let temp = colors[0];
+                colors[0] = colors[2];
+                colors[2] = temp;
+            }
+            let nbShift = authorid % 3;
+            while(nbShift > 0){
+                let temp = colors[0];
+                colors[0] = colors[1];
+                colors[1] = colors[2];
+                colors[2] = temp;
+                nbShift--;
+            }
+            let rgb = parseInt('0x' + parseInt(colors[0], 16) + parseInt(colors[1], 16) + parseInt(colors[2], 16)); //make color based on user ID
+
+            message.channel.send("embed").then(msg => {
+                let latency = msg.createdTimestamp - message.createdTimestamp
+                var embed = new Discord.RichEmbed()
+                    .setTitle('🏓 **Pong!**')
+                    .setDescription("**Latency**\n" + latency + "ms\n**API**\n" + APIdelay + 'ms')
+                    .setColor(rgb)
+                    .setFooter(message.author.username, message.author.avatarURL);
+                msg.edit({embed});
                 msg.react('❌');
             });
         }
